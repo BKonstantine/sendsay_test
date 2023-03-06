1a079eacfc14a11f484b87ffb6bfcaff7b90217e
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-06 19:55:01 +0300

    feat: add operators-block

--- src/components/builder/builder.tsx
+++ src/components/builder/builder.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import style from "./builder.module.css";
 import Display from "../display/display";
+import OperatorsBlock from "../operators-block/operators-block";
 
 const Builder: FC = () => {
   return (
     <div className={style.builder}>
-      <Display result={0}/>
+      <Display result={0} />
+      <OperatorsBlock />
     </div>
   );
 };
---
+++ src/components/operators-block/operators-block.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import style from "./builder.module.css";
+import Button from "../button/button";
+
+const OperatorsBlock: FC = () => {
+  return <div className={style.operators}></div>;
+};
+
+export default OperatorsBlock;
