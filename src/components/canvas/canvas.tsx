4476ecf23d6f9b91511e562b66d19c8dc92d9441
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-06 17:48:28 +0300

    feat: add builder and canvas component

---
+++ src/components/canvas/canvas.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import style from "./canvas.module.css";
+
+const Canvas: FC = () => {
+  return <div className={style.canvas}></div>;
+};
+
+export default Canvas;
---
+++ src/components/constructor/builder.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import style from "./builder.module.css";
+
+const Builder: FC = () => {
+  return <div className={style.builder}></div>;
+};
+
+export default Builder;
