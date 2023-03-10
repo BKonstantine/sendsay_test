62c99bd922925d3c81511195aa5fcdd8fb7fb5a4
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-10 12:38:56 +0300

    feat: add style and markup for canvas component

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -1,8 +1,21 @@
 import { FC } from "react";
 import style from "./canvas.module.css";
+import info from "../../image/icon-canvas.svg";
 
 const Canvas: FC = () => {
-  return <div className={style.canvas}></div>;
+  return (
+    <div className={style.canvas}>
+      <div className={style.info}>
+        <img
+          src={info}
+          alt="Иконка абстрактного рисунка"
+          className={style.image}
+        />
+        <p className={style.title}>Перетащите сюда</p>
+        <p className={style.subtitle}>любой элемент из&nbsp;левой панели</p>
+      </div>
+    </div>
+  );
 };
 
 export default Canvas;
