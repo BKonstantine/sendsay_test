7c1bf706f06bbd6e919aa36d926ee94005be82ac
Kostya Buzunov <buzunov.constantine@yandex.ru>
2023-03-10 13:55:35 +0300

    feat: add dnd provider

--- src/components/calculator-constructor/calculator-constructor.tsx
+++ src/components/calculator-constructor/calculator-constructor.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import style from "./calculator-constructor.module.css";
 import Canvas from "../canvas/canvas";
 import Builder from "../builder/builder";
 
 const CalculatorConstructor: FC = () => {
   return (
-    <div className={style.container}>
-      <Builder />
-      <Canvas />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className={style.container}>
+        <Builder />
+        <Canvas />
+      </div>
+    </DndProvider>
   );
 };
 
